6ca8ad807971fd9698ef861d637db782e290f821
“Anyalee610” <anayalambright610@gmail.com>
2022-08-31 15:38:27 -0400

    feat: added divs for the the dialogue

--- Start/start.js
+++ Start/start.js
@@ -1,17 +1,17 @@
 const startButton = document.getElementById('start')
 const firstScreen = document.getElementById('name')
-const dialoge = document.getElementById('dialogePage')
+const dialogue = document.getElementById('dialoguePage')
 const boy = document.getElementById('boy')
 const girl = document.getElementById('girl')
 
 
 
-dialoge.style.display = "none"
+dialogue.style.display = "none"
 
 startButton.addEventListener('click', function(){
     firstScreen.style.display = "none";
     boy.src = 'boy.png'
     girl.src = 'girl.png';
-    dialoge.style.display = "inline";
+    dialogue.style.display = "inline";
     
 })
